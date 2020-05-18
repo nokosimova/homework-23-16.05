179881f1c11ac6d531935bfc873b352f051c2227
kosimovanodira <nkosimova@gmail.com>
2020-05-18 23:57:41 +0500

    Добавьте файлы проекта.

---
+++ hw23-SOLID/CoffeeShop.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace hw23_SOLID
+{
+    class Drink
+    {
+        string Name { get; set; }
+        decimal Price { get; set; }
+    }
+    class CoffeeShop
+    {
+        List<Drink> Menu = new List<Drink>();
+
+    }
+}
---
+++ hw23-SOLID/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace hw23_SOLID
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
